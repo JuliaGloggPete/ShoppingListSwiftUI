1fa0459774c8df732b359d8760aa2393ee7679a7
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-04-17 09:44:16 +0200

    Initial Commit

---
+++ Shoppinglist/ShoppinglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppinglistApp.swift
+//  Shoppinglist
+//
+//  Created by Julia Petersson  on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct ShoppinglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
